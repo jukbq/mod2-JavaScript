f7329834a4166d969706400b5d6abd2f052122bc
Yurii Ohlii <jukbq77@gmail.com>
2022-02-17 15:12:41 +0200

    ,od2hw1

---
+++ mod2hw1/main.js
@@ -0,0 +1,24 @@
+// Завдання 1
+
+let city = "Київ";
+city = "Львів";
+let address  = city;
+alert(address);
+
+// Завдання 2
+
+let n = 2;
+let result = Math.pow(n, 3);
+console.log(result);
+
+//Завдання 3
+
+let penPrice = 5.25;
+let penQuantity = 4;
+let pencilPrice = 3.50;
+let pencilQuantity = 6;
+let summ = (penPrice * penQuantity) + (pencilPrice * pencilQuantity);
+document.write(summ);
+
+
+
